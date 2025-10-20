1eb2a8a3191a2456951d1d72fe5029d3ef9e00fc
anthocab2 <anthonycaban1010@gmail.com>
2025-10-19 21:59:02 -0400

    Task 1: print_alphabet function

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Description: uses _putchar to print letters from a to z,
+ * followed by a new line
+ */
+void print_alphabet(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
