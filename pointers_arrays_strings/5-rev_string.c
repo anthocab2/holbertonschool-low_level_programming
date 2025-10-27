f4288bdb8251d11bb440595b7c2479506e06ab67
anthocab2 <anthonycaban1010@gmail.com>
2025-10-27 02:19:15 -0400

    Implement rev_string function to reverse a string in place

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * rev_string - Reverses a string.
+ * @s: The string to be reversed.
+ *
+ * Return: void
+ */
 void rev_string(char *s)
 {
 int i, len;
@@ -7,10 +13,10 @@ char temp;
 
 /* Contar la longitud de la cadena */
 len = 0;
-while (s[len] != '\0') /* NO usar // en C90 */
+while (s[len] != '\0')
 len++;
 
-/*Intercambiar caracteres de los extremos hasta el centro */
+/* Intercambiar caracteres de los extremos hasta el centro */
 for (i = 0; i < len / 2; i++)
 {
 temp = s[i];
