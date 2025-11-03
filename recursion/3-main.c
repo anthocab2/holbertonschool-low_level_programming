8f76c7983242ccec3449208cfac2ea1597518a4a
anthocab2 <anthonycaban1010@gmail.com>
2025-11-02 21:58:54 -0400

    Task 3: factorial

---
+++ recursion/3-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("%d\n", factorial(5));
+    printf("%d\n", factorial(0));
+    printf("%d\n", factorial(-1));
+    return (0);
+}
