857fc8a972008389e3beda41067289b5bad3df4f
anthocab2 <anthonycaban1010@gmail.com>
2025-11-02 21:59:50 -0400

    Task 4: _pow_recursion

---
+++ recursion/4-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("%d\n", _pow_recursion(2, 5));
+    printf("%d\n", _pow_recursion(2, 0));
+    printf("%d\n", _pow_recursion(2, -1));
+    return (0);
+}
