f29b6a64cc1b5942b15980847b2f808cc0ad875f
anthocab2 <anthonycaban1010@gmail.com>
2025-11-07 11:58:51 -0400

    3-mul: multiplies two numbers passed as arguments

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -2,12 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
-* main - multiplies two numbers
-* @argc: number of arguments
-* @argv: array of arguments
-* Return: 0 on success, 1 on error
-*/
 int main(int argc, char *argv[])
 {
 int num1, num2, result;
