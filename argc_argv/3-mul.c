910da42c99495f593ce64e7a7bc0ac723fb2df9b
anthocab2 <anthonycaban1010@gmail.com>
2025-11-07 11:57:01 -0400

    3-mul: multiplies two numbers passed as arguments

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+int num1, num2, result;
+
+if (argc != 3)  /* Debemos tener exactamente 2 argumentos además del nombre */
+{
+printf("Error\n");
+return (1);
+}
+
+/* Convertimos las cadenas a enteros */
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+
+/* Multiplicación */
+result = num1 * num2;
+
+printf("%d\n", result);
+
+return (0);
+}
