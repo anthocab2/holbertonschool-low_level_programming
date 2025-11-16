c4d56e65aa818f99b6c920bd5deb8601ec724613
anthocab2 <anthonycaban1010@gmail.com>
2025-11-16 19:58:09 -0400

    Add int_index function for searching integer using function pointers

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-/* Prototype of the function that takes a function pointer */
-void print_name(char *name, void (*f)(char *));
+#include <stddef.h>
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* FUNCTION_POINTERS_H */
