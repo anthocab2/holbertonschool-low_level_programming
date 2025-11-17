c68420eb9904abffcd54c6823efd52c5c19ee196
anthocab2 <anthonycaban1010@gmail.com>
2025-11-16 21:04:25 -0400

    Add array_iterator function and main to test it

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,9 +1,8 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include <stddef.h>
+#include <stddef.h> /* size_t */
 
 void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* FUNCTION_POINTERS_H */
