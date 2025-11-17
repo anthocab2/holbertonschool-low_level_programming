f1327511aff2b3ae92c0a0f18fdeebc048e310ab
anthocab2 <anthonycaban1010@gmail.com>
2025-11-16 21:48:53 -0400

    Implement print_all function to print all types from variadic args

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -5,51 +5,49 @@
 /* Struct to map type to its print function */
 typedef struct printer
 {
-char type;
-void (*f)(va_list);
+    char type;
+    void (*f)(va_list);
 } printer_t;
 
 /* Print functions for each type */
 void print_char(va_list args)
 {
-printf("%c", va_arg(args, int));
+    printf("%c", va_arg(args, int));
 }
 
 void print_int(va_list args)
 {
-printf("%d", va_arg(args, int));
+    printf("%d", va_arg(args, int));
 }
 
 void print_float(va_list args)
 {
-printf("%f", va_arg(args, double));
+    printf("%f", va_arg(args, double));
 }
 
 void print_string(va_list args)
 {
-char *str = va_arg(args, char *);
-if (!str)
-str = "(nil)";
-printf("%s", str);
+    char *str = va_arg(args, char *);
+    printf("%s", str ? str : "(nil)");
 }
 
 /* Main function for task 3 */
 void print_all(const char * const format, ...)
 {
-va_list args;
-unsigned int i = 0, j;
-char *sep = "";
+    va_list args;
+    unsigned int i = 0, j;
+    char *sep = "";
 
-printer_t printers[] = {
-{'c', print_char},
-{'i', print_int},
-{'f', print_float},
-{'s', print_string}
-};
+    printer_t printers[] = {
+        {'c', print_char},
+        {'i', print_int},
+        {'f', print_float},
+        {'s', print_string}
+    };
 
-va_start(args, format);
+    va_start(args, format);
 
-if (format) /* primer if */
+ if (format) /* primer if */
 {
 while (format[i])
 {
