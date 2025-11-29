f59f5a6fb745b8e6d685601927e1470e665ddfcf
anthocab2 <anthonycaban1010@gmail.com>
2025-11-29 19:18:32 -0400

    4.Free list

--- singly_linked_lists/4-main.c
+++ singly_linked_lists/4-main.c
@@ -1,19 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "lists.h"
 
 /**
-* main - check free_list
-*
-* Return: Always 0.
-*/
+ * main - check free_list
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-list_t *head = NULL;
+list_t *head = malloc(sizeof(list_t));
+list_t *second;
 
-add_node_end(&head, "Hello");
-add_node_end(&head, "World");
+head->str = strdup("Hello");
+head->len = 5;
+
+second = malloc(sizeof(list_t));
+second->str = strdup("World");
+second->len = 5;
+
+head->next = second;
+second->next = NULL;
 
 free_list(head);
-return (0);
+
+return 0;
 }
