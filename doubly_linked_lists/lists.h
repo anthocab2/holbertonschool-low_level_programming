d405cd2fc85b202d2926ba25717b79f9a28f4096
anthocab2 <anthonycaban1010@gmail.com>
2025-11-29 22:11:32 -0400

    1. Return number of nodes in doubly linked list

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -5,8 +5,7 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-/**
-* struct dlistint_s - doubly linked list
+/** * struct dlistint_s - doubly linked list
 * @n: integer
 * @prev: points to the previous node
 * @next: points to the next node
@@ -19,5 +18,6 @@ struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
 #endif /* LISTS_H */
