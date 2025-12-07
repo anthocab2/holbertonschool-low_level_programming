a04abff4f477218204f9333ac904955708004400
anthocab2 <anthonycaban1010@gmail.com>
2025-12-07 15:07:29 -0400

    Task 4: Add main.c for hash_table_get test

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_get - Retrieves a value associated with a key
+* @ht: Hash table
+* @key: Key to look for
+*
+* Return: Value associated with key, or NULL if not found
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+unsigned long int index;
+hash_node_t *tmp;
+
+if (!ht || !key || *key == '\0')
+return (NULL);
+
+index = key_index((const unsigned char *)key, ht->size);
+tmp = ht->array[index];
+
+while (tmp)
+{
+if (strcmp(tmp->key, key) == 0)
+return (tmp->value);
+tmp = tmp->next;
+}
+
+return (NULL);
+}
