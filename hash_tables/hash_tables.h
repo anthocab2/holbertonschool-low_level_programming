64b0f896f65cac9b9476ce385c14e146e39fd708
anthocab2 <anthonycaban1010@gmail.com>
2025-12-07 12:25:56 -0400

    Task 1: Implement djb2 hash function

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -6,27 +6,27 @@
 #include <stdio.h>
 
 /**
- * struct hash_node_s - Node of a hash table
- * @key: The key
- * @value: The value
- * @next: Pointer to next node
- */
+* struct hash_node_s - Node of a hash table
+* @key: The key
+* @value: The value
+* @next: Pointer to next node
+*/
 typedef struct hash_node_s
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
+char *key;
+char *value;
+struct hash_node_s *next;
 } hash_node_t;
 
 /**
- * struct hash_table_s - Hash table data structure
- * @size: Size of the array
- * @array: Array of pointers to linked lists
- */
+* struct hash_table_s - Hash table data structure
+* @size: Size of the array
+* @array: Array of pointers to linked lists
+*/
 typedef struct hash_table_s
 {
-	unsigned long int size;
-	hash_node_t **array;
+unsigned long int size;
+hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
