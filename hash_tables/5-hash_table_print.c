6ecccc45a9fd54ea4e967a0f2626726a588d4c2e
anthocab2 <anthonycaban1010@gmail.com>
2025-12-07 15:08:44 -0400

    Task 5: Implement hash_table_print

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_print - Prints a hash table
+* @ht: Hash table to print
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+unsigned long int i;
+hash_node_t *tmp;
+int first = 1;
+
+if (!ht)
+return;
+
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+tmp = ht->array[i];
+while (tmp)
+{
+if (!first)
+printf(", ");
+printf("'%s': '%s'", tmp->key, tmp->value);
+first = 0;
+tmp = tmp->next;
+}
+}
+printf("}\n");
+}
